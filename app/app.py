5c5415088d42997465c8d1afa178f532269cd5a9
Onda Reki <77719187+reki204@users.noreply.github.com>
2022-03-20 15:30:18 +0900

    run.pyでサーバー起動

--- app/app.py
+++ app/app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8000, debug=True)
+# @app.route("test")
+# def test():
+#   return print('test')
